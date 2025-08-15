fd2b47f84c47d67b1c065d85251094eb08a173ad
Lingeswari <Lingeswarisundar@gmail.com>
2025-08-15 11:34:39 +0530

    Array size calculating program completed

---
+++ ArraySize.java
@@ -0,0 +1,8 @@
+class ArraySize  {
+     public static void main(String[] args){
+         int[] a = new int[10];
+         int[] b = new int[20];
+         a = b;
+         System.out.println(a.length);
+     } 
+}
