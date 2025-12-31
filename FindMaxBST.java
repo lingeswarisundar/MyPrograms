5f925b318962d4d8325ae1df580bca8804944191
Lingeswari <Lingeswarisundar@gmail.com>
2025-12-31 14:18:57 +0530

    Finding maximum value in BSTree

---
+++ FindMaxBST.java
@@ -0,0 +1,49 @@
+class BinaryTree {
+    int data;
+    BinaryTree left;
+    BinaryTree right;
+    BinaryTree (int data){
+        this.data = data;
+    }
+}
+class FindMaxBST {
+    static BinaryTree insert(int data,BinaryTree root) {
+        if(root==null) {
+            root = new BinaryTree(data);
+        } else if(data < root.data){
+            root.left = insert(data,root.left);
+        } else {
+            root.right = insert(data,root.right);
+        }
+        return root;
+    }
+    static void inorder(BinaryTree root) {
+        if(root!=null){
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+    }
+    static int findMaxBST(BinaryTree root) {
+        if (root.right == null) {
+            return root.data;
+        }
+        return findMaxBST(root.right);
+    }
+    public static void main(String[] args) {
+        BinaryTree ro = null;
+        BinaryTree root = insert(50,ro);
+        insert(40,root);
+        insert(60,root);
+        insert(30,root);
+        insert(45,root);
+        insert(55,root);
+        insert(70,root);
+        insert(51,root);
+        System.out.println("\tBinarySearchTree");
+        System.out.print("Inorder ");
+        inorder(root);
+        System.out.println("\nMaximum value in the BStree is "+findMaxBST(root));
+    }
+}
+
