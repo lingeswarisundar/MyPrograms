2f5e1eb4492df9d541da501290115c623d8f38f6
Lingeswari <Lingeswarisundar@gmail.com>
2026-06-03 13:53:56 +0530

    LeetCode 283

---
+++ MoveZeroes.java
@@ -0,0 +1,16 @@
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int len = nums.length;
+        while(right<len) {
+            if(nums[right]!=0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+            right++;
+        }    
+    }
+}
