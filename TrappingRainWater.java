dd1b20dc3a9868e206982058d7be0b17a4cbfc8b
Lingeswari <Lingeswarisundar@gmail.com>
2026-06-06 16:05:40 +0530

    LeetCode 42

---
+++ TrappingRainWater.java
@@ -0,0 +1,26 @@
+class TrappingRainWater {
+    public int trap(int[] height) {
+        int len = height.length;
+        int left = 0;
+        int right = len-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+        if (len <= 2) {
+            return 0;
+        }
+        while (left < right) {
+            leftMax = Math.max(leftMax,height[left]);
+            rightMax = Math.max(rightMax,height[right]);
+            if (leftMax < rightMax) {
+                water += leftMax - height[left];
+                left++;
+            } else {
+                water += rightMax - height[right];
+                right--;
+            }   
+            
+        }
+        return water;
+    }
+}
